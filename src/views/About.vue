e0164cdd05925d3236b83da2db12151c2a3f6c50
maurizWeymann <s0528279@htw-berlin.de>
2021-12-05 20:54:51 +0100

    add static.json

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,11 +2,6 @@
   <div class="about">
     <h1>This is an about page</h1>
   </div>
-  <div id="shopping-list">
-    <h1>{{header}}</h1>
-    <input type="text" v-model="header">
-  </div>
 </template>
 <script>
-    el: '#shopping-list';
 </script>
