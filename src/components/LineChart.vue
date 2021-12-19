8d0adb67c033462ed2cd47c91a67bf9544f5461f
maurizWeymann <s0528279@htw-berlin.de>
2021-12-19 19:51:17 +0100

    display more than one line chart

--- src/components/LineChart.vue
+++ src/components/LineChart.vue
@@ -19,15 +19,25 @@ export default {
           id: 'vuechart-example'
         },
         xaxis: {
-          categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
+          categories: this.xValues
         }
       },
       series: [
         {
           name: 'series-1',
-          data: [30, 40, 35, 50, 49, 60, 70, 1]
+          data: this.yValues
         }
       ]
+    }
+  },
+  props: {
+    xValues: {
+      type: Array,
+      required: true
+    },
+    yValues: {
+      type: Array,
+      required: true
     }
   }
 }
--- src/views/Calculations.vue
+++ src/views/Calculations.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="calculations">
     <h1>This is an calculation page</h1>
-    <line-chart></line-chart>
+    <line-chart v-for="(dataSet, index) in datasets" :x-values="dataSet.xValues" :y-values="dataSet.yValues" :key="index"></line-chart>
   </div>
 </template>
 
@@ -10,7 +10,21 @@ import LineChart from '@/components/LineChart.vue'
 
 export default {
   name: 'Calculations',
-  components: { LineChart }
+  components: { LineChart },
+  data () {
+    return {
+      datasets: [
+        {
+          xValues: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998],
+          yValues: [30, 40, 35, 50, 49, 60, 70, 91]
+        },
+        {
+          xValues: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998],
+          yValues: [30, 40, 35, 50, 49, 60, 70, 91]
+        }
+      ]
+    }
+  }
 }
 
 </script>
