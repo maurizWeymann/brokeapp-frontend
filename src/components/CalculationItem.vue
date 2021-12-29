55b594f6b0304cd80d8cec831d7c0ae67254b530
maurizWeymann <s0528279@htw-berlin.de>
2021-12-29 17:31:08 +0100

    calculates live no additional contribution

--- src/components/CalculationItem.vue
+++ src/components/CalculationItem.vue
@@ -4,23 +4,23 @@
       <div class="row align-items-center">
         <div class="col-auto my-1">
           <label class="mr-sm-2" >initial investment</label>
-          <input type="text" class="form-control" placeholder="1000,00" v-model="initialInvestment" @keypress="calculate">
+          <input type="number" class="form-control" placeholder="1000,00" v-model="initialInvestment" @change="calculate">
         </div>
         <div class="col-auto my-1">
           <label class="mr-sm-2" >years to accumulate</label>
-          <input type="text" class="form-control" placeholder="10" v-model="yearsToAccumulate" @keypress="calculate">
+          <input type="number" class="form-control" placeholder="10" v-model="yearsToAccumulate" @change="calculate">
         </div>
         <div class="col-auto my-1">
           <label class="mr-sm-2" >additional contribution</label>
-          <input type="text" class="form-control" placeholder="500,00">
+          <input type="number" class="form-control" placeholder="500,00" v-model="additionalContribution" @change="calculate">
         </div>
       </div>
       <div class="col-auto my-1">
         <div class="col-auto my-1">
           <label class="mr-sm-2" >interest rate</label>
-          <input type="text" class="form-control" placeholder="10%">
+          <input type="number" class="form-control" placeholder="10%" v-model="interestRate" @change="calculate">
         </div>
-        <label class="mr-sm-2" >compound crequency</label>
+        <label class="mr-sm-2" >compound frequency</label>
         <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
           <option selected>Choose...</option>
           <option value="daily">Daily</option>
@@ -47,18 +47,23 @@ export default {
   },
   data () {
     return {
-      initialInvestment: 0,
-      yearsToAccumulate: 0,
-      xValues: [1, 2, 3],
-      yValues: [1, 2, 3]
+      initialInvestment: this.initialInvestment,
+      yearsToAccumulate: this.yearsToAccumulate,
+      additionalContribution: this.additionalContribution,
+      interestRate: this.interestRate,
+      xValues: this.xValues,
+      yValues: this.yValues
     }
   },
   methods: {
     calculate () {
-      const result = calculate(this.initialInvestment, this.yearsToAccumulate)
+      const result = calculate(this.initialInvestment, this.yearsToAccumulate, this.interestRate)
       this.xValues = result.xValues
       this.yValues = result.yValues
+      console.log(`x:  ${this.xValues} `)
       console.log(this.xValues)
+      console.log(`y:  ${this.yValues} `)
+      console.log(this.yValues)
     }
   }
 }
--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,7 +1,27 @@
-export default function calculate (initialInvestment, yearToAccumulate) {
-  // ...
+export default function calculate (initialInvestment, yearToAccumulate, interestRate) {
+  // let initialInvestment
+  // let yearsToAccumulate
+  let xes = []
+  let yes = []
+  if (interestRate === 0) {
+    interestRate = 0
+  }
+  const oneTenthYearToAccumulate = yearToAccumulate / 10
+  for (let i = 0; i < 10; i++) {
+    xes[i] = oneTenthYearToAccumulate * i
+  }
+  if (initialInvestment > 0) {
+    yes.push(initialInvestment)
+    for (let i = 1; i <= yearToAccumulate; i++) {
+      yes[i] = yes[i - 1] + (yes[i - 1] * interestRate / 100)
+      // xes[i] = i
+    }
+  } else {
+    yes = [1, 2, 3]
+    xes = [100, 200, 300, 400, 5, 6]
+  }
   return {
-    xValues: [1, 2, 3, 4, 5],
-    yValues: [1, 2, 3, 4, 5]
+    xValues: xes,
+    yValues: yes
   }
 }
