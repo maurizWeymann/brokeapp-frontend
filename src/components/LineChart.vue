0a1afafe12ff5428732c700ed864afd91c93ae3a
maurizWeymann <s0528279@htw-berlin.de>
2022-01-09 14:01:28 +0100

    fix stroke to straight

--- src/components/LineChart.vue
+++ src/components/LineChart.vue
@@ -21,6 +21,9 @@ export default {
             show: false
           }
         },
+        stroke: {
+          curve: 'straight'
+        },
         dataLabels: {
           enabled: false
         },
