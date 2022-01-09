e2c9b7ca44f26093fadacdda2444792ae0fc350a
maurizWeymann <s0528279@htw-berlin.de>
2022-01-09 13:47:13 +0100

    created test calculator.spec.js

--- tests/unit/calculator.spec.js
+++ tests/unit/calculator.spec.js
@@ -4,11 +4,13 @@ describe('calculation', () => {
   it('return an calculation object', () => {
     const wrapper = calculate(100, 21, 100, 'daily')
     expect(wrapper).valueOf('yValues: [100, 400, 1600, 6400, 25600, 204800, 819200, 3276800, 13107200, 52428800, 209715200]')
-    console.log(wrapper)
+  })
+  it('checks if calculation returns a xValues', () => {
+    const wrapper = calculate(100, 21, 100, 'daily')
+    expect(wrapper).toHaveProperty('xValues')
   })
   it('return the total earning', () => {
-    const wrapper = calculate(100, 21, 100, 'daily')
-    expect(wrapper).valueOf('yValues: [100, 400, 1600, 6400, 25600, 204800, 819200, 3276800, 13107200, 52428800, 209715200]')
-    console.log(wrapper)
+    const wrapper = calculate(100, 27, 10, 'daily')
+    expect(wrapper.yValues[10]).toBe(1311)
   })
 })
