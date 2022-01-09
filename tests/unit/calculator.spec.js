2bf1920bb12e4692c2b07b4a3303f059f8470ce7
maurizWeymann <s0528279@htw-berlin.de>
2022-01-09 13:26:34 +0100

    created test calculator.spec.js

--- tests/unit/calculator.spec.js
+++ tests/unit/calculator.spec.js
@@ -1,6 +1,11 @@
 import calculate from '@/components/calculator'
 
 describe('calculation', () => {
+  it('return an calculation object', () => {
+    const wrapper = calculate(100, 21, 100, 'daily')
+    expect(wrapper).valueOf('yValues: [100, 400, 1600, 6400, 25600, 204800, 819200, 3276800, 13107200, 52428800, 209715200]')
+    console.log(wrapper)
+  })
   it('return the total earning', () => {
     const wrapper = calculate(100, 21, 100, 'daily')
     expect(wrapper).valueOf('yValues: [100, 400, 1600, 6400, 25600, 204800, 819200, 3276800, 13107200, 52428800, 209715200]')
